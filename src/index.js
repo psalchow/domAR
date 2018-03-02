10202a110cf1d7c7addb1848f0517a3c66fd2539
Oliver Widder <oliver.widder@gmail.com>
2018-03-02 08:33:06 +0100

    slide3dd3 -> dbar

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {initCubes} from './cubes/cubesIndex';
 import {initRadars} from './radar/radarsIndex';
 import {initSlides} from './slides/slidesIndex';
 import {demoSlides} from "./slides/demoSlides/demoSlides";
-import * as slide3dd3 from "./slides/3dd3/slides3dd3";
+import * as dbar from "./slides/dbar/dbar";
 
 import {appendStyles} from './util/loadStyles';
 
@@ -18,9 +18,9 @@ window.simplAR = simplAR;
 const BODY_TYPE_RADAR = "radar";
 const BODY_TYPE_CUBE = "cube";
 const BODY_TYPE_SLIDE_DEMO = "slideDemo";
-const BODY_TYPE_SLIDE_3DD3 = "slide3dd3";
+const BODY_TYPE_SLIDE_DBAR = "dbar";
 
-const bodyType = query.firstParamSet([BODY_TYPE_SLIDE_DEMO, BODY_TYPE_CUBE, BODY_TYPE_RADAR, BODY_TYPE_SLIDE_3DD3]);
+const bodyType = query.firstParamSet([BODY_TYPE_SLIDE_DEMO, BODY_TYPE_CUBE, BODY_TYPE_RADAR, BODY_TYPE_SLIDE_DBAR]);
 const paramValue = query.paramValue(bodyType);
 
 switch (bodyType) {
@@ -39,8 +39,8 @@ switch (bodyType) {
         initSlides("#container", demoSlides, paramValue);
         break;
 
-    case BODY_TYPE_SLIDE_3DD3:
-        initSlides("#container", slide3dd3.init, paramValue);
+    case BODY_TYPE_SLIDE_DBAR:
+        initSlides("#container", dbar.init, paramValue);
 
     default:
         // do nothing
