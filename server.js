b524a95a4bc97f82a71af907048d10dbfafaefa3
Oliver Widder <oliver.widder@gmail.com>
2018-03-12 15:15:46 +0100

    server.js now w/o https

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
 var express = require('express');
 var http = require('http');
-var https = require('https');
-var pem = require('pem');
 
 const commandServer = require('./serverSrc/slidAR/commandServer');
 
-const wsPort = process.argv[4] || 2337;
-const httpPort = process.argv[3] || 2338;
-const folder = process.argv[2] || 'build';
 
-pem.createCertificate({ days:1, selfSigned:true }, function(err, keys) {
-    // Create middleware for a static server
-    var app = express();
-    // app.all('*', (req, res, next) => {
-    //     // always redirect to https
-    //     if (req.secure) return next();
-    //     res.redirect('https://' + req.hostname + ':' + httpsServer.address().port + req.url);
-    // });
+function startServer() {
+    const wsPort = process.argv[4] || 2337;
+    const httpPort = process.argv[3] || 2338;
+    const folder = process.argv[2] || 'build';
+
+    const app = express();
     app.use('/', express.static(__dirname + '/' + folder));
 
-    // Create an HTTPS service
-    var httpsServer = https.createServer({
-        key: keys.serviceKey,
-        cert: keys.certificate
-    }, app).listen(0);
-
-    // Create an http service
     http.createServer(app).listen(httpPort);
 
-    commandServer.startNew(wsPort);
-
     console.log(`serving folder '${folder}' on port: ${httpPort}`);
-});
+
+    commandServer.startNew(wsPort);
+}
+
+startServer();
