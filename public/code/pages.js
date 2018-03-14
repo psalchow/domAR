ded1b8a920a0035891d36d324c00dacc2ac8f137
thr0n <hendrik.thorun@gmail.com>
2018-03-14 11:13:33 +0100

    add workaround for unknown reference error

--- public/code/pages.js
+++ public/code/pages.js
@@ -2,21 +2,31 @@
 
 (function () {
     if (window.location.href.indexOf("qrTarget") > 1) {
+
         // check for qr-code defined param
-        const target = simplAR.paramValue("qrTarget");
+        // FIXME dirty hack! We should check why simplAR is undefined at this point.
+        try {
+            const target = simplAR.paramValue("qrTarget");
+        } catch (error) {
+            window._pages = [
+                {id: "html1", name: "stocks/lufthansa.html"},
+                {id: "html2", name: "stocks/microsoft.html"},
+                {id: "html3", name: "stocks/prosiebensat1.html"}
+            ];
+        }
 
         console.log("got something: " + target);
 
         switch (target) {
             case "logo":
                 window._pages = [
-                    {id: "html1", name: "logo/html1.html"},
-                    {id: "html2", name: "logo/html2.html"},
-                    {id: "html3", name: "logo/html3.html"},
-                    {id: "html4", name: "logo/html4.html"},
-                    {id: "html5", name: "logo/html5.html"},
-                    {id: "html6", name: "logo/html6.html"},
-                    {id: "html7", name: "logo/html7.html"},
+                    {id: "html1", name: "logo/logo1.html"},
+                    {id: "html2", name: "logo/logo2.html"},
+                    {id: "html3", name: "logo/logo3.html"},
+                    {id: "html4", name: "logo/logo4.html"},
+                    {id: "html5", name: "logo/logo5.html"},
+                    {id: "html6", name: "logo/logo6.html"},
+                    {id: "html7", name: "logo/logo7.html"},
                 ];
                 break;
             case "schedule":
@@ -26,9 +36,9 @@
                 break;
             case "stocks":
                 window._pages = [
-                    {id: "html1", name: "stocks/html1.html"},
-                    {id: "html2", name: "stocks/html2.html"},
-                    {id: "html3", name: "stocks/html3.html"}
+                    {id: "html1", name: "stocks/lufthansa.html"},
+                    {id: "html2", name: "stocks/microsoft.html"},
+                    {id: "html3", name: "stocks/prosiebensat1.html"}
                 ];
                 break;
             default:
